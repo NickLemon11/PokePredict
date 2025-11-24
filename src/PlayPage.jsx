f5763701c9062cd3353acd4a64eeb6158b8ac0ef
NickLemon11 <Nicksolonolo@gmail.com>
2025-11-24 18:50:51 -0500

    Opponent_Integration

--- src/PlayPage.jsx
+++ src/PlayPage.jsx
@@ -3,21 +3,48 @@ import { PokemonSelect } from "./PokemonSelect"
 import "./play.css";
 
 export default function Play() {
+  const [player, setPlayer] = useState("Your Team");
+
 
   return (
     <>
-    <div className="user-pokemon">
-      <PokemonSelect/>
-      <PokemonSelect />
+
+    <button className="menu-button" onClick={() => {player == "Your Team" ? setPlayer("Opponent Team") : setPlayer("Your Team")}}>{player}</button>
+    
+    <div className={player == "Your Team" ? "show" : "hide"}>
+      <div className="pokemon-info">
+        <PokemonSelect player="user"/>
+        <PokemonSelect player="user"/>
+      </div>
+
+      <div className="pokemon-info" id="pok-spacer">
+        <PokemonSelect player="user"/>
+        <PokemonSelect player="user"/>
+      </div>
+        <div className="pokemon-info" id="pok-spacer">
+        <PokemonSelect player="user"/>
+        <PokemonSelect player="user"/>
+      </div>
     </div>
-    <div className="user-pokemon" id="pok-spacer">
-      <PokemonSelect/>
-      <PokemonSelect />
-    </div>
-      <div className="user-pokemon" id="pok-spacer">
-      <PokemonSelect/>
-      <PokemonSelect />
-    </div>
+
+    <div className={player == "Opponent Team" ? "show" : "hide"}>
+      <div className="pokemon-info">
+        <PokemonSelect player="opponent"/>
+        <PokemonSelect player="opponent"/>
+      </div>
+
+      <div className="pokemon-info" id="pok-spacer">
+        <PokemonSelect player="opponent"/>
+        <PokemonSelect player="opponent"/>
+      </div>
+
+        <div className="pokemon-info" id="pok-spacer">
+        <PokemonSelect player="opponent"/>
+        <PokemonSelect player="opponent"/>
+      </div>
+    </div> 
+
+    
 
       <div className="bottom-content"></div>
     </>
